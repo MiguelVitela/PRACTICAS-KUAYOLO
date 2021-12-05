a65673362f346b57653c0d3107e11ca92f3b01d8
MiguelVitela <71278199+MiguelVitela@users.noreply.github.com>
2021-12-05 15:53:42 -0600

    Add files via upload

---
+++ Javascript programming course/SECCIÓN 1 CONSTRUIR UNA APP CONTADORA DE PASAJEROS/LECCIÓN 9/LECCIÓN 9 Usar funciones para escribir menos codigo.js
@@ -0,0 +1,14 @@
+/* Para ejecutar una función es necesario crear la funcion primero y después mandarla llamar
+como se muestra en el siguiente ejemplo el cual simulará un juego de carreras. */
+
+// Creación de la función
+function cuenta() {
+    console.log("5")
+    console.log("4")
+    console.log("3")
+    console.log("2")
+    console.log("1")
+}
+
+// LLamado de la función
+cuenta()
---
+++ Javascript programming course/SECCIÓN 1 CONSTRUIR UNA APP CONTADORA DE PASAJEROS/contador.js
@@ -0,0 +1,12 @@
+let contador = 0;
+
+console.log(contador)
+
+
+/* Ejercicio 1
+
+1: Crear una variable y colocar en ella mi edad.*/
+let edad = 20;
+
+//2: cargar ese dato en consola.
+console.log(edad);
